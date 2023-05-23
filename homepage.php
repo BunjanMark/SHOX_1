05ba296268bd0841e7645cb685aacdff5d73714f
symondlibago <125746176+symondlibago@users.noreply.github.com>
2023-05-24 01:46:44 +0800

    Add files via upload

---
+++ homepage.php
@@ -0,0 +1,35 @@
+<?php include('common/header.php') ?>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+<body>
+<div class="home-div">
+	</div>
+
+	<div class="wrapper">
+			<h1>SLIS</h1>
+
+			<ul class="dynamic-txt">
+				<li><span>NEW LEARNING ENVIRONMENT IN THIS DIGITAL WORLD.</span></li>
+			</ul>
+
+		</div>
+
+		<div class="w-100 in-ad-ap">
+			<div class="row m-auto text-center">
+				<div class="col-md-4"><h3>INTRODUCING-CHMSC</h3></div>
+				<div class="col-md-4"><h3>ADMISSION POLICY</h3></div>
+				<div class="col-md-4"><h3>APPLY NOW</h3></div>
+			</div>
+		</div>
+		<div class="paragraph">
+			<p>
+				CHMSC provides a harmonious environment and learning opportunities  to its students regardless of their gender, socioeconomic background, religious beliefs and regional differences.
+			</p>
+		</div>
+		
+
+
+	<?php include('common/cards.php') ?>
+	<?php include('common/footer.php') ?>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,20 +1,88 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+            width: 100vw;
+            height: 100vh;
+            background-color: #eee;
+        }
 
-	<?php include('common/header.php') ?>
-	<div class="home-div"></div>
-		<div class="w-100 in-ad-ap">
-			<div class="row m-auto text-center">
-				<div class="col-md-4"><h3>INTRODUCING-CHMSC</h3></div>
-				<div class="col-md-4"><h3>ADMISSION POLICY</h3></div>
-				<div class="col-md-4"><h3>APPLY NOW</h3></div>
-			</div>
-		</div>
-		<div class="paragraph">
-			<p>
-				CHMSC provides a harmonious environment and learning opportunities  to its students regardless of their gender, socioeconomic background, religious beliefs and regional differences.
-			</p>
-		</div>
-	<?php include('common/cards.php') ?>
-	<?php include('common/footer.php') ?>
+        .content {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            width: 100%;
+            height: 100%;
+        }
+
+        .loader-wrapper {
+            width: 100%;
+            height: 100%;
+            position: fixed;
+            top: 0;
+            left: 0;
+            background-color: #5e0808;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            z-index: 9999;
+        }
+
+        .loader {
+            display: inline-block;
+            width: 30px;
+            height: 30px;
+            position: relative;
+            border: 4px solid #fff;
+            animation: loader 2s infinite ease;
+        }
+
+        .loader-inner {
+            vertical-align: top;
+            display: inline-block;
+            width: 100%;
+            background-color: #fff;
+            animation: loader-inner 2s infinite ease-in;
+        }
+
+        @keyframes loader {
+            0% { transform: rotate(0deg); }
+            25% { transform: rotate(180deg); }
+            50% { transform: rotate(180deg); }
+            75% { transform: rotate(360deg); }
+            100% { transform: rotate(360deg); }
+        }
+
+        @keyframes loader-inner {
+            0% { height: 0%; }
+            25% { height: 0%; }
+            50% { height: 100%; }
+            75% { height: 100%; }
+            100% { height: 0%; }
+        }
+    </style>
+</head>
+<body>
+    <div class="content">
+        <div class="loader-wrapper">
+            <span class="loader"><span class="loader-inner"></span></span>
+        </div>
+    </div>
+
+    <script>
+    $(window).on("load", function() {
+        setTimeout(function() {
+            $(".loader-wrapper").fadeOut("slow", function() {
+                // After the loader fades out, redirect to the homepage
+                window.location.href = 'homepage.php';
+            });
+        }, 2000); // Adjust the delay time (in milliseconds) as needed
+    });
+</script>
+
 </body>
 </html>
-
